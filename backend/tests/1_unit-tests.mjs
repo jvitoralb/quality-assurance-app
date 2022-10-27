5751b6c73a10c96690e79d96d8d44505e5406027
jvitoralb <jvvalb@gmail.com>
2022-10-27 16:50:17 -0300

    fix tests

--- backend/tests/1_unit-tests.mjs
+++ backend/tests/1_unit-tests.mjs
@@ -1,52 +1,66 @@
-import chai from 'chai'
-import ConvertHandler from '../controllers/convertHandler.js'
+import chai, { expect } from 'chai'
+import ConvertHandler, { convertInputTests } from '../controllers/convertHandler.js'
 
 
 let assert = chai.assert
-let handler = new ConvertHandler()
-console.log('unit-tests')
-suite('Input Tests', () => {
+// console.log('unit-tests')
+let testHandler = new ConvertHandler()
+suite('Unit Tests', () => {
+
     test('Input Validation', () => {
-        assert.strictEqual(handler.getValue('20kg'), 20)
-        assert.strictEqual(handler.getValue('3.1kg'), 3.1)
-        assert.strictEqual(handler.getValue(''), 1)
-        assert.isNumber(handler.getValue('1/2kg'))
-        assert.isNull(handler.getValue('1/2/2kg'))
+        testHandler.getInput('20kg')
+        assert.strictEqual(testHandler.getValue(), 20)
+        testHandler.getInput('3.1kg')
+        assert.strictEqual(testHandler.getValue(), 3.1)
+        testHandler.getInput('kg')
+        assert.strictEqual(testHandler.getValue(), 1)
+        testHandler.getInput('1/2kg')
+        assert.isNumber(testHandler.getValue())
+        testHandler.getInput('1/2/2kg')
+        assert.throws(testHandler.getValue, Error, 'invalid number')
     })
 
-    const validUnitsTest = handler.units
+    const validUnitsTest = testHandler.units
 
     test('Unit Validation', () => {
-        assert.isOk(handler.getUnit('kg'))
-        assert.isNull(handler.getUnit('jg'))
-        validUnitsTest.forEach(unit => assert.strictEqual(handler.getUnit(unit[0]), unit[0]))
         validUnitsTest.forEach(unit => {
-            handler.getUnit(unit[0])
-            assert.strictEqual(handler.getFullUnit(unit[1]), unit[1])
+            testHandler.getInput(unit[0])
+            assert.strictEqual(testHandler.getUnit(), unit[0])
+        })
+
+        testHandler.inputUnit = ''
+        testHandler.getInput('10ty')
+        assert.throws(testHandler.getUnit, Error, 'invalid unit')
+
+        validUnitsTest.forEach(unit => {
+            let result = convertInputTests(unit[0])
+            assert.strictEqual(result.returnUnit, unit[2])
+        })
+
+        validUnitsTest.forEach(unit => {
+            let result = convertInputTests(unit[0])
+            assert.strictEqual(result.unitName, unit[1])
         })
     })
-})
-
-suite('Conversion Tests', () => {
     test('gal and L Tests', () => {
-        handler.getInput('20gal')
-        assert.strictEqual(handler.convert(), '75.7L')
-        handler.getInput('75.7L')
-        assert.strictEqual(handler.convert(), '20gal')
+        testHandler.getInput('20gal')
+        assert.strictEqual(testHandler.convert(), '75.7082L')
+        testHandler.getInput('75.7L')
+        assert.strictEqual(testHandler.convert(), '19.997gal')
     })
     test('Km and Mile Tests', () => {
-        handler.getInput('10km')
-        let [ miles ] = handler.convert().split('mi', 1)
+        testHandler.getInput('10km')
+        let [ miles ] = testHandler.convert().split('mi', 1)
         assert.approximately(Number(miles), 6.2, 0.02)
 
-        handler.getInput('6.2mi')
-        let [ kilometers ] = handler.convert().split('km', 1)
+        testHandler.getInput('6.2mi')
+        let [ kilometers ] = testHandler.convert().split('km', 1)
         assert.approximately(Number(kilometers), 10, 0.03)
     })
     test('Kg and Pound Tests', () => {
-        handler.getInput('15kg')
-        assert.strictEqual(handler.convert(), '33.075lbs')
-        handler.getInput('33.075lbs')
-        assert.strictEqual(handler.convert(), '15kg')
+        testHandler.getInput('15kg')
+        assert.strictEqual(testHandler.convert(), '33.075lbs')
+        testHandler.getInput('33.075lbs')
+        assert.strictEqual(testHandler.convert(), '15kg')
     })
 })
