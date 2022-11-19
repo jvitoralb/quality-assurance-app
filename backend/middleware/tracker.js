96ecdce1a9408a64f802913f639549b1746ca801
jvitoralb <jvvalb@gmail.com>
2022-11-19 15:41:22 -0300

    minor changes

--- backend/middleware/tracker.js
+++ backend/middleware/tracker.js
@@ -2,14 +2,14 @@ import CustomError from '../errors/custom.js'
 
 
 const checkValidBody = (req, res, next) => {
-    let { project_name, project_id, ...update } = req.body
+    let { project_name, issue_id, ...update } = req.body
 
-    if (!project_id) {
+    if (!issue_id) {
         throw new CustomError('missing _id', 400)
     }
 
     if (Object.values(update).every(val => !val)) {
-        throw new CustomError('no update field(s) sent', 400, { _id: project_id })
+        throw new CustomError('no update field(s) sent', 400, { _id: issue_id })
     }
 
     next()
--- backend/routes/tracker.js
+++ backend/routes/tracker.js
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import fcctesting from './fcctesting.cjs'
-import { Project } from '../models/tracker.js';
+import { Project, Issue } from '../models/tracker.js'
+import checkValidBody from '../middleware/tracker.js'
+import { trackerHandler, getAllIssues, updateHandler } from '../controllers/tracker.js'
 
 
 const issueTracker = Router();
@@ -9,46 +11,28 @@ issueTracker.get('/', (req, res) => {
     res.status(200).send('Hello Issue Tracker')
 })
 
-issueTracker.get('/api/issues/:project', (req, res) => {
-    // Get all issues on a project
-    res.status(200).send(`Project Name: ${req.params.project}`)
-    // add filter to the project - can use a middleware - can have multiple filters
+issueTracker.get('/api/issues/:project', (req, res, next) => {
+    getAllIssues(req, res, next)
 })
 
-issueTracker.post('/api/issues/:project', (req, res) => {
-    const saveDoc = async () => {
-        const { project_name } = req.body
-        try {
-            const saved = await new Project({
-                name: project_name
-            }).save()
-            return saved
-        } catch(err) {
-            console.log(err, '<--- Save doc err ---')
-        }
-    }
-    const result = saveDoc()
-    res.status(201).json({
-        project: result.name,
-        ...req.body
+issueTracker.post('/api/issues/:project', (req, res, next) => {
+    trackerHandler(req, res, next)
+})
+
+issueTracker.put('/api/issues/:project', checkValidBody, (req, res, next) => {
+    updateHandler(req, res, next)
+})
+
+issueTracker.delete('/api/issues/:project', async (req, res) => {
+    const project_deletions = await Project.deleteMany({})
+    const issues_deletions = await Issue.deleteMany({})
+    console.log({
+        project_deletions,
+        issues_deletions
     })
-})
-
-issueTracker.put('/api/issues/:project', (req, res) => {
-    //update multiple fields 
-    // update issue with missing _id (??)
-    // update issue with no fields to update (??)
-    // update issue with invalid _id (??)
-    res.status(201).json({
-        ...req.body
-    })
-})
-
-issueTracker.delete('/api/issues/:project', (req, res) => {
-    //delete issue with invalid _id (??)
-    //delete issue with missing _id (??)
     res.status(204).json({
-        issue_deleted: req.body.project
+        project_deletions,
+        issues_deletions
     })
 })
 
