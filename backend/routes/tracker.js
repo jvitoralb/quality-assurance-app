046cf78fc1ff72f2a66a805c5ceb4f208a4d41fa
jvitoralb <jvvalb@gmail.com>
2022-11-20 15:39:43 -0300

    added final routes to tracker

--- backend/routes/tracker.js
+++ backend/routes/tracker.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import fcctesting from './fcctesting.cjs'
-import { Project, Issue } from '../models/tracker.js'
-import checkValidBody from '../middleware/tracker.js'
-import { trackerHandler, getAllIssues, updateHandler } from '../controllers/tracker.js'
+import checkValidBody, { validateDeletion } from '../middleware/tracker.js'
+import {
+    createIssues, getAllIssues,
+    updateIssues, deleteIssues, getAllProjects
+} from '../controllers/tracker.js'
 
 
 const issueTracker = Router();
@@ -11,29 +13,24 @@ issueTracker.get('/', (req, res) => {
     res.status(200).send('Hello Issue Tracker')
 })
 
+issueTracker.get('/api/projects', (req, res, next) => {
+    getAllProjects(req, res, next)
+})
+
 issueTracker.get('/api/issues/:project', (req, res, next) => {
     getAllIssues(req, res, next)
 })
 
 issueTracker.post('/api/issues/:project', (req, res, next) => {
-    trackerHandler(req, res, next)
+    createIssues(req, res, next)
 })
 
 issueTracker.put('/api/issues/:project', checkValidBody, (req, res, next) => {
-    updateHandler(req, res, next)
+    updateIssues(req, res, next)
 })
 
-issueTracker.delete('/api/issues/:project', async (req, res) => {
-    const project_deletions = await Project.deleteMany({})
-    const issues_deletions = await Issue.deleteMany({})
-    console.log({
-        project_deletions,
-        issues_deletions
-    })
-    res.status(204).json({
-        project_deletions,
-        issues_deletions
-    })
+issueTracker.delete('/api/issues/:project', validateDeletion, (req, res, next) => {
+    deleteIssues(req, res, next)
 })
 
 fcctesting(issueTracker)
