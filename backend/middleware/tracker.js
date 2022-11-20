e722fc61c9bafd58604d32a73570b2a4352ef386
jvitoralb <jvvalb@gmail.com>
2022-11-20 15:39:13 -0300

    added new middleware to tracker

--- backend/middleware/tracker.js
+++ backend/middleware/tracker.js
@@ -1,6 +1,15 @@
 import CustomError from '../errors/custom.js'
 
 
+export const validateDeletion = (req, res, next) => {
+    // I don't really like this
+    if (!req.body.issue_id) {
+        throw new CustomError('missing _id', 400)
+    }
+
+    next()
+}
+
 const checkValidBody = (req, res, next) => {
     let { project_name, issue_id, ...update } = req.body
 
