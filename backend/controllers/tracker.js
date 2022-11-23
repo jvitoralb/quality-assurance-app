caaaa22657b94b3514948cff565e431207a29f65
jvitoralb <jvvalb@gmail.com>
2022-11-23 11:17:28 -0300

    improving controller code

--- backend/controllers/tracker.js
+++ backend/controllers/tracker.js
@@ -5,74 +5,65 @@ import CustomError from '../errors/custom.js'
 
 
 class IssuesTracker {
-    constructor(issueVals, project) {
-        this.issueVals = issueVals
-        this.project = project
+    constructor(docIssue, docProject) {
+        this.docIssue = docIssue
+        this.docProject = docProject
     }
 
     issueDelete = async () => {
-        try {
-            this.issueVals = await Issue.findByIdAndDelete(this.issueVals._id)
-            this.projectFind() // this doesnt have a await key because there isn't anything to push
-            this.projectUpdate(true)
-            return this.issueVals
-        } catch(err) {
-            throw err
+        let deleted = await Issue.findByIdAndDelete(this.docIssue._id)
+
+        if (!deleted) {
+            // throw new CustomError('could not delete', 200, { _id: issue_id || _id })
+            throw new CustomError('could not delete', 400, { _id: this.docIssue._id })
         }
+
+        this.docIssue = deleted
+        await this.projectUpdate(true)
     }
 
     projectFind = async (filters) => {
-        try {
-            this.project = await Project.findOne({ name: this.project.name })
-            .select('-__v').populate({
-                path: 'issues',
-                select: '-__v -project',
-                match: filters || null
-            })
+        let found = await Project.findOne({ name: this.docProject.name })
+        .select('-__v').populate({
+            path: 'issues',
+            select: '-__v -project',
+            match: filters || null
+        })
 
-            return this.project
-        } catch(err) {
-            throw err
+        if (!found) {
+            throw new CustomError('could not find project', 400, { name: this.docProject.name })
         }
-    }
 
-    issueFind = async () => {
-        try {
-            return await Issue.findById(this.issueVals._id)
-            .select('-__v')
-        } catch(err) {
-            throw err
-        }
+        this.docProject = found
     }
 
     projectUpdate = async (remove) => {
-        console.log('trying to update')
-        try {
-            await Project.findOneAndUpdate({ _id: this.project._id }, {
-                [remove ? '$pull' : '$push']: {
-                    issues: ObjectId(this.issueVals._id)
-                }
-            }, { new: true })
-        } catch(err) {
-            throw err
-        }
+        this.docProject = await Project.findOneAndUpdate({ name: this.docProject.name }, {
+            [remove ? '$pull' : '$push']: {
+                issues: ObjectId(this.docIssue._id)
+            }
+        }, { new: true })
     }
 
     issueUpdate = async () => {
-        const { project_name, ...update } = this.issueVals
+        const { project_name, _id, ...update } = this.docIssue
 
         try {
-            this.issueVals = await Issue.findByIdAndUpdate(this.issueVals._id, {
+            this.docIssue = await Issue.findByIdAndUpdate(_id, {
                 $set: {
                     ...update,
                     updated_on: new Date()
                 }
             }, { new: true }).select('-__v')
 
-            return this.issueVals
+            if (!this.docIssue) {
+                // throw new CustomError(`could not update`, 200, { _id })
+                throw new CustomError(`could not update`, 400, { _id })
+            }
         } catch(err) {
             if (err.name === 'CastError') {
-                throw new CustomError(`could not update`, 400, { _id: this.issueVals._id })
+                // throw new CustomError(`could not update`, 200, { _id })
+                throw new CustomError(`could not update`, 400, { _id })
             }
             throw err
         }
@@ -81,17 +72,18 @@ class IssuesTracker {
     issueSave = async () => {
         const newIssue = new Issue({
             _id: new mongoose.Types.ObjectId,
-            project: this.project._id,
+            project: this.docProject._id,
             created_on: new Date(),
             updated_on: new Date(),
-            ...this.issueVals
+            ...this.docIssue
         })
 
         try {
-            this.issueVals = await newIssue.save()
+            this.docIssue = await newIssue.save()
             await this.projectUpdate()
         } catch(err) {
             if (err.name === 'ValidationError') {
+                // throw new CustomError('required field(s) missing', 200)
                 throw new CustomError('required field(s) missing', 400)
             }
             throw err
@@ -99,12 +91,10 @@ class IssuesTracker {
     }
 
     projectSave = async () => {
-        const newProject = new Project({
-            name: this.project.name
-        })
+        const newProject = new Project(this.docProject)
 
-        try {
-            this.project = await newProject.save()
+        try {// see if it is a problem all info stored in class
+            this.docProject = await newProject.save()
         } catch(err) {
             if (err.code === 11000) {
                 await this.projectFind()
@@ -119,16 +109,11 @@ export const deleteIssues = async (req, res, next) => {
     const { params, body: { issue_id, _id, ...rest } } = req
     const trackerRef = new IssuesTracker({ _id: issue_id || _id }, { name: params.project })
 
-    try {
-        const deleted = await trackerRef.issueDelete()
-
-        if (!deleted) {
-            throw new CustomError('could not delete', 400, { _id: issue_id || _id })
-        }
-
+    try { // see if it is a problem all info stored in class
+        await trackerRef.issueDelete()
         res.status(200).json({
             result: 'successfully deleted',
-            _id: deleted._id
+            _id: trackerRef.docIssue._id
         })
     } catch(err) {
         next(err)
@@ -138,19 +123,12 @@ export const deleteIssues = async (req, res, next) => {
 export const updateIssues = async (req, res, next) => {
     const { params, body: { issue_id, _id, ...rest } } = req
     const trackerRef = new IssuesTracker({ _id: issue_id || _id, rest }, { name: params.project })
-        // if the update field comes undefined it updates 
-        // it should not
-    try {
-        const updated = await trackerRef.issueUpdate()
-        // reset all info stored in trackerRef
-        // First see if it is a problem
-        if (!updated) {
-            throw new CustomError(`could not update`, 400, { _id: issue_id || _id })
-        }
 
+    try { // see if it is a problem all info stored in class
+        await trackerRef.issueUpdate()
         return res.status(200).json({
             result: 'successfully updated',
-            _id: updated._id
+            _id: trackerRef.docIssue._id
         })
     } catch(err) {
         next(err)
@@ -158,9 +136,6 @@ export const updateIssues = async (req, res, next) => {
 }
 
 export const getAllProjects = async (req, res, next) => {
-    const { body, params } = req
-    const trackerRef = new IssuesTracker(body, { name: params.project })
-
     try {
         const docs = await Project.find({})
         .select('-__v')
@@ -174,11 +149,10 @@ export const getAllProjects = async (req, res, next) => {
 export const getAllIssues = async (req, res, next) => {
     const { body, params, query } = req
     const trackerRef = new IssuesTracker(body, { name: params.project })
-
-    try {
-        const { issues } = await trackerRef.projectFind(query)
-
-        res.status(200).json(issues)
+    // issue_id doesnt work here
+    try { // see if it is a problem all info stored in class
+        await trackerRef.projectFind(query)
+        res.status(200).json(trackerRef.docProject.issues)
     } catch(err) {
         next(err)
     }
@@ -188,13 +162,12 @@ export const createIssues = async (req, res, next) => {
     const { params, body: { project_name, ...rest } } = req
     const trackerRef = new IssuesTracker(rest, { name: params.project })
 
-    try {
+    try { // see if it is a problem all info stored in class
         await trackerRef.projectSave()
         await trackerRef.issueSave()
-        const doc = await trackerRef.issueFind()
-        // reset all info stored in trackerRef
-        // First see if it is a problem
-        res.status(201).json(doc)
+        const { _doc: { __v, ...rest } } = trackerRef.docIssue
+
+        res.status(201).json(rest)
     } catch(err) {
         next(err)
     }
