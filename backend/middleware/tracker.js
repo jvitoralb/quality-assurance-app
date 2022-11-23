044375dd667f49907528d399287888760dff8234
jvitoralb <jvvalb@gmail.com>
2022-11-22 23:20:20 -0300

    fix conflicts with fcc testing

--- backend/middleware/tracker.js
+++ backend/middleware/tracker.js
@@ -3,7 +3,9 @@ import CustomError from '../errors/custom.js'
 
 export const validateDeletion = (req, res, next) => {
     // I don't really like this
-    if (!req.body.issue_id) {
+    let { issue_id, _id } = req.body
+
+    if (!issue_id && !_id) {
         throw new CustomError('missing _id', 400)
     }
 
@@ -11,14 +13,14 @@ export const validateDeletion = (req, res, next) => {
 }
 
 const checkValidBody = (req, res, next) => {
-    let { project_name, issue_id, ...update } = req.body
+    let { project_name, issue_id, _id, ...update } = req.body
 
-    if (!issue_id) {
+    if (!issue_id && !_id) {
         throw new CustomError('missing _id', 400)
     }
 
     if (Object.values(update).every(val => !val)) {
-        throw new CustomError('no update field(s) sent', 400, { _id: issue_id })
+        throw new CustomError('no update field(s) sent', 400, { _id: issue_id || _id })
     }
 
     next()
