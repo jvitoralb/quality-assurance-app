ff27ee3b4dd7f11ef2507195be06cdfccf177066
jvitoralb <jvvalb@gmail.com>
2022-12-18 15:12:52 -0300

    finish personal-library middlewares

--- backend/middleware/library.js
+++ backend/middleware/library.js
@@ -3,24 +3,35 @@ import { Book } from '../models/library.js'
 
 
 export const checkOnBooks = async (req, res, next) => {
+    const { _id } = req.params
+
     try {
-        const report = await Book.exists({ _id: req.params._id })
+        const report = await Book.exists({ _id })
 
         if (report === null) {
-            throw new CustomError('no book exists', 400, { _id: req.params._id })
+            throw new CustomError('no book exists', 200, { _id }) // FCC
         }
+
+        next()
     } catch(err) {
+        if (err.kind == 'ObjectId') {
+            err = new CustomError('no book exists', 200, { _id }) // FCC
+        }
         next(err)
+    }
+}
+
+export const checkText = (req, res, next) => {
+    if (!req.body.text.trim()) {
+        throw new CustomError('missing required field comment', 200) // FCC
     }
 
     next()
 }
 
-export const validBookTitle = (req, res, next) => { //personalLibrary.post('/api/books'
-    const { title } = req.body
-
-    if (!title) {
-        throw new CustomError('missing required field title', 400)
+export const checkTitle = (req, res, next) => {
+    if (!req.body.title) {
+        throw new CustomError('missing required field title', 200) // FCC
     }
 
     next()
--- backend/routes/library.js
+++ backend/routes/library.js
@@ -1,7 +1,11 @@
 import { Router } from 'express'
 import fcctesting from './fcctesting.cjs'
+import { checkTitle, checkText, checkOnBooks } from '../middleware/library.js'
 import { libraryHandleGet, libraryHandlePost, libraryHandleDelete } from '../controllers/library.js'
 
+/**
+ * Need to improve this code
+**/
 
 const personalLibrary = Router();
 
@@ -14,28 +18,25 @@ personalLibrary.get('/api/books', (req, res, next) => {
     // Gets all books
     libraryHandleGet(req, res, next)
 })
-personalLibrary.get('/api/books/:_id', (req, res, next) => {
-    // Gets a specific book
-    console.log('get')
-    libraryHandleGet(req, res, next)
-})
-
-
-personalLibrary.post('/api/books', (req, res, next) => {
+personalLibrary.post('/api/books', checkTitle, (req, res, next) => {
     // Posts a book
     libraryHandlePost(req, res, next)
 })
-personalLibrary.post('/api/books/:_id', (req, res, next) => {
-    // Posts a comment to a book
-    libraryHandlePost(req, res, next)
-})
-
-
 personalLibrary.delete('/api/books', (req, res, next) => {
     // Deletes all books
     libraryHandleDelete(req, res, next)
 })
-personalLibrary.delete('/api/books/:_id', (req, res, next) => {
+
+
+personalLibrary.get('/api/books/:_id', checkOnBooks, (req, res, next) => {
+    // Gets a specific book
+    libraryHandleGet(req, res, next)
+})
+personalLibrary.post('/api/books/:_id', checkOnBooks, checkText, (req, res, next) => {
+    // Posts a comment to a book
+    libraryHandlePost(req, res, next)
+})
+personalLibrary.delete('/api/books/:_id', checkOnBooks, (req, res, next) => {
     // Deletes a book and its comments
     libraryHandleDelete(req, res, next)
 })
