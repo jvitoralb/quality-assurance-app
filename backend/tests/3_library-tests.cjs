8de7f3861efac41969c46c99db7024539a168ade
jvitoralb <jvvalb@gmail.com>
2022-12-18 15:14:01 -0300

    library tests changes

--- backend/tests/3_library-tests.cjs
+++ backend/tests/3_library-tests.cjs
@@ -1,11 +1,16 @@
+// require('dotenv').config()
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 
+
+// if(process.env.PROJECT_TEST !== 'library') return
 
 chai.use(chaiHttp)
 
 suite('Library Functional Tests', async () => {
     const app = (await import('../app.js')).default
+    // const dbConnection = (await import('../connection.js')).default
+    // dbConnection()
     let assert = chai.assert
 
     suite('Routing tests', () => {
@@ -19,12 +24,12 @@ suite('Library Functional Tests', async () => {
             })
         })
         suite('POST', () => {
-            let validId
+            let validId = ''
 
             test('#With title', (done) => {
                 chai.request(app)
                 .post('/personal-library/api/books')
-                .send({ title: 'A volta dos que não foram'})
+                .send({ title: 'A volta dos que não foram' })
                 .end((err, res) => {
                     assert.strictEqual(res.status, 201)
                     assert.strictEqual(res.body.title, 'A volta dos que não foram')
@@ -38,7 +43,7 @@ suite('Library Functional Tests', async () => {
                 .post('/personal-library/api/books')
                 .send({ })
                 .end((err, res) => {
-                    assert.strictEqual(res.status, 400)
+                    assert.strictEqual(res.status, 200)// FCC
                     assert.deepEqual(res.body, { error: 'missing required field title' }) //separate whats missing from error
                     done()
                 })
@@ -46,31 +51,31 @@ suite('Library Functional Tests', async () => {
             test('#With Comment', (done) => {
                 chai.request(app)
                 .post('/personal-library/api/books/' + validId)
-                .send({ comment: 'This book is really good!' })
+                .send({ text: 'This book is really good!' })
                 .end((err, res) => {
                     assert.strictEqual(res.status, 201)
-                    assert.containsAllKeys(res.body, ['_id', 'title', 'comments'])
-                    assert.strictEqual(res.body.comments.at(-1), 'This book is really good!')
+                    assert.containsAllKeys(res.body[0], ['_id', 'title', 'comments'])
+                    assert.strictEqual(res.body[0].comments.at(-1).text, 'This book is really good!')
                     done()
                 })
             })
             test('#Without Comment', (done) => {
                 chai.request(app)
                 .post('/personal-library/api/books/' + validId)
-                .send({ })
+                .send({ text: '' })
                 .end((err, res) => {
-                    assert.strictEqual(res.status, 400)
-                    assert.deepEqual(res.body, { error: 'missing required field comment'}) //separate whats missing from error
+                    assert.strictEqual(res.status, 200)// FCC
+                    assert.deepEqual(res.body, { error: 'missing required field comment'})
                     done()
                 })
             })
             test('#With Invalid _id', (done) => {
                 chai.request(app)
                 .post('/personal-library/api/books/invalid_id')
-                .send({ comment: 'This book is amazing!!' })
+                .send({ text: 'This book is amazing!!' })
                 .end((err, res) => {
-                    assert.strictEqual(res.status, 400)
-                    assert.deepEqual(res.body, { error: 'no book exists' }) // I should assert for _id in the error object as well
+                    assert.strictEqual(res.status, 200)// FCC
+                    assert.deepEqual(res.body, { error: 'no book exists', _id: 'invalid_id' })
                     done()
                 })
             })
@@ -92,58 +97,66 @@ suite('Library Functional Tests', async () => {
             })
             test('#Book With Invalid _id', (done) => {
                 chai.request(app)
-                .get('/personal-library/api/book/invalid_id')
+                .get('/personal-library/api/books/invalid_id')
                 .end((err, res) => {
-                    assert.strictEqual(res.status, 400)
-                    assert.deepEqual(res.body, { error: 'no book exists' })//I should assert for _id in the error object as well
+                    assert.strictEqual(res.status, 200)// FCC
+                    assert.deepEqual(res.body, { error: 'no book exists', _id: 'invalid_id' })
                     done()
                 })
             })
-            test('#Book With Valid _id', (done) => {
-                let validId
+            let validId = ''
+            test('#Post The getter - A book', (done) => {
                 chai.request(app)
                 .post('/personal-library/api/books')
                 .send({ title: 'The getter - A book' })
                 .end((err, res) => {
-                    assert.strictEqual(res.status, 201, 'Book to delete created!')
+                    assert.strictEqual(res.status, 201)
                     validId = res.body._id
+                    assert.strictEqual(res.body._id, validId)
+                    done()
                 })
-
+            })
+            test('#Book With Valid _id', (done) => {
                 chai.request(app)
                 .get('/personal-library/api/books/' + validId)
                 .end((err, res) => {
                     assert.strictEqual(res.status, 200)
-                    assert.containsAllKeys(res.body, ['_id', 'title', 'comments'])
-                    assert.isArray(res.body.comments)
+                    assert.containsAllKeys(res.body[0], ['_id', 'title', 'comments'])
+                    assert.isArray(res.body[0].comments)
                     done()
                 })
             })
         })
         suite('DELETE', () => {
-            test('#Book With Valid _id', (done) => {
-                let deleteId
+            let deleteId = ''
+            test('#Create book to delete', (done) => {
                 chai.request(app)
                 .post('/personal-library/api/books')
                 .send({ title: 'The book - Deletion' })
                 .end((err, res) => {
                     assert.strictEqual(res.status, 201, 'Book to delete created!')
                     deleteId = res.body._id
+                    assert.strictEqual(res.body._id, deleteId, 'deleteId should equal body._id')
+                    done()
                 })
-
+            })
+            test('#Book With Valid _id', (done) => {
                 chai.request(app)
-                .delete('/personal-library/api/books')
+                .delete('/personal-library/api/books/' + deleteId)
                 .end((err, res) => {
                     assert.strictEqual(res.status, 200)
-                    assert.deepEqual(res.body, { message: 'delete successful' })//I should assert for the title as well
+                    assert.strictEqual(res.body.message, 'delete successful')
+                    assert.strictEqual(res.body._id, deleteId)
+                    assert.hasAllDeepKeys(res.body, ['message', '_id', 'deletedCount'])
                     done()
                 })
             })
             test('#Book With Invalid _id', (done) => {
                 chai.request(app)
-                .delete('/personal-library/api/books/not_valid_id')
+                .delete('/personal-library/api/books/invalid_id')
                 .end((err, res) => {
-                    assert.strictEqual(res.status, 400)
-                    assert.deepEqual(res.body, { error: 'no book exists' })//I should assert for _id in the error object as well
+                    assert.strictEqual(res.status, 200)// FCC
+                    assert.deepEqual(res.body, { error: 'no book exists', _id: 'invalid_id' })
                     done()
                 })
             })
@@ -152,10 +165,11 @@ suite('Library Functional Tests', async () => {
                 .delete('/personal-library/api/books')
                 .end((err, res) => {
                     assert.strictEqual(res.status, 200)
-                    assert.deepEqual(res.body, { message: 'complete delete successful' })
+                    assert.strictEqual(res.body.message, 'complete delete successful')
+                    assert.hasAllDeepKeys(res.body, ['message', 'deletedCount'])
                     done()
                 })
             })
         })
     })
-})
+}).timeout(5000)
