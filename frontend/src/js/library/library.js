583ff1663d3c7423b0118a1cd64c42df2f99e546
jvitoralb <jvvalb@gmail.com>
2023-03-15 16:06:58 -0300

    update: mv handlers to a single file

--- frontend/src/js/library/library.js
+++ frontend/src/js/library/library.js
@@ -2,15 +2,15 @@ import createHTMLElem from '../utils/domElements.js';
 import initFormsListeners from './libraryForms.js';
 import handleAPICalls from './libraryApi.js';
 
-const bookContainer = document.querySelector('#books-container');
-const commentsList = document.querySelector('#book-modal-comments');
-const bookInfoModalElem = document.querySelector('#book-info-modal');
-const addBookModalElem = document.querySelector('#add-book-modal');
-const bookInfoModal = new bootstrap.Modal(bookInfoModalElem);
-const addBookModal = new bootstrap.Modal(addBookModalElem);
+export const bookContainer = document.querySelector('#books-container');
+export const commentsList = document.querySelector('#book-modal-comments');
+export const bookInfoModalElem = document.querySelector('#book-info-modal');
+export const addBookModalElem = document.querySelector('#add-book-modal');
+export const bookInfoModal = new bootstrap.Modal(bookInfoModalElem);
+export const addBookModal = new bootstrap.Modal(addBookModalElem);
 
 
-const displayBookInfo = ({ bookTitle, bookAuthor,  commentNodes }) => {
+export const displayBookInfo = ({ bookTitle, bookAuthor,  commentNodes }) => {
     commentsList.removeChild(document.querySelector('#spinner-container-book-info'));
 
     document.querySelector('#title-book-info').textContent = bookTitle;
@@ -22,7 +22,7 @@ const displayBookInfo = ({ bookTitle, bookAuthor,  commentNodes }) => {
 }
 
 
-const createCommentElement = ({ _id, created_by, created_on, text }) => {
+export const createCommentElement = ({ _id, created_by, created_on, text }) => {
     let li = createHTMLElem['listItems']('', 'list-group-item d-flex', _id);
     li.appendChild(createHTMLElem['image']('img-thumbnail', '../../../public/images/ursinho-pooh.png', 'pooh-profile-picture', 64));
 
@@ -45,7 +45,7 @@ const createCommentElement = ({ _id, created_by, created_on, text }) => {
     return li;
 }
 
-const createBookElement = (bookObj) => {
+export const createBookElement = (bookObj) => {
     let bookElem = createHTMLElem['anchor']('p-2 col-lg-3 col-md-4 col-sm-6 border', bookObj._id);
     let commentsCount = bookObj.commentcount > 0 ? `Comments: ${bookObj.commentcount}` : '+ Add Comment';
     bookElem.innerHTML = `${bookObj.title}<br/>Author: ${bookObj.author}<br/>${commentsCount}`;
@@ -64,121 +64,6 @@ const createSpinner = () => {
     div.appendChild(createHTMLElem['span']('visually-hidden'));
     return container;
 }
-
-
-export const errorHandler = (dataError, source) => {
-    const allBooksError = (errorAnswer) => {
-        let text = createHTMLElem['paragraph'](errorAnswer.error, 'm-1 text-center');
-        text.setAttribute('id', 'no-books-text');
-        bookContainer.removeChild(document.querySelector('#spinner-container-all-books'));
-        bookContainer.appendChild(text);
-    }
-
-    const bookInfoError = (errorAnswer) => {
-        displayBookInfo({
-            bookTitle: errorAnswer.error,
-            bookAuthor: '',
-            commentNodes: []
-        });
-    }
-
-    const addBookError = (errorAnswer) => {
-        const errorMessageBook = document.querySelector('#add-book-error-message');
-
-        const dismissMessageBook = () => errorMessageBook.textContent = '';
-
-        errorMessageBook.textContent = errorAnswer.error.slice(0, 1).toUpperCase() + errorAnswer.error.slice(1) + '!';
-        addBookModalElem.addEventListener('click', dismissMessageBook);
-    }
-
-    const addCommentError = (errorAnswer) => {
-        const errorMessageComment = document.querySelector('#book-info-error-message');
-    
-        const dismissMessageComment = () => errorMessageComment.textContent = '';
-
-        errorMessageComment.textContent = errorAnswer.error.slice(0, 1).toUpperCase() + errorAnswer.error.slice(1) + '!';
-        bookInfoModalElem.addEventListener('click', dismissMessageComment);
-    }
-
-    const answerCallback = {
-        'get-all-books': allBooksError,
-        'get-book-info': bookInfoError,
-        'add-book-form': addBookError,
-        'add-comment-form': addCommentError
-    }
-    answerCallback[source](dataError);
-}
-
-
-export const answerHandler = (dataAnswer, source) => {
-    const addBookAnswer = (answer) => {
-        if (document.querySelector('#no-books-text')) {
-            bookContainer.removeChild(document.querySelector('#no-books-text'));
-        }
-
-        let elem = createBookElement(answer);
-        bookContainer.insertBefore(elem, bookContainer.firstChild);
-        addBookModal.hide();
-    }
-
-    const addCommentAnswer = (answer) => {
-        const anchors = document.querySelectorAll('a');
-        for(let i = 0; i < anchors.length; i++) {
-            if (anchors[i].getAttribute('href') === answer[0]._id) {
-                anchors[i].innerHTML = `${answer[0].title}<br/>Author: ${answer[0].author}<br/>Comments: ${answer[0].commentcount}`;
-                break;
-            }
-        }
-    
-        const latestComment = answer[0].comments[answer[0].comments.length - 1];
-        let newComment = createCommentElement(latestComment);
-    
-        const listLine = commentsList.firstChild;
-        commentsList.insertBefore(newComment, listLine.nextSibling);
-    }
-
-    const allBooksAnswer = (answer) => {
-        if (document.querySelector('#spinner-container-all-books')) {
-            bookContainer.removeChild(document.querySelector('#spinner-container-all-books'));
-        }
-
-        answer.forEach((book) => {
-            let elem = createBookElement(book);
-            bookContainer.appendChild(elem);
-        });
-    }
-
-    const bookInfoAnswer = (answer) => {
-        const book = answer[0];
-        const commentSecNodes = [];
-
-        document.querySelector('#add-comment-form').setAttribute('action', book._id);
-    
-        let listLine = createHTMLElem['listItems']('', 'list-group-item mx-1 p-0', 'comment-first-li');
-        commentSecNodes.push(listLine);
-    
-        book.comments.sort((a, b) => new Date(b.created_on).getTime() - new Date(a.created_on).getTime())
-        .forEach((com) => {
-            let listElem = createCommentElement(com);
-            commentSecNodes.push(listElem);
-        });
-    
-        displayBookInfo({
-            bookTitle: book.title,
-            bookAuthor: book.author,
-            commentNodes: commentSecNodes
-        });
-    }
-
-    const answerCallback = {
-        'get-all-books': allBooksAnswer,
-        'get-book-info': bookInfoAnswer,
-        'add-book-form': addBookAnswer,
-        'add-comment-form': addCommentAnswer
-    }
-    answerCallback[source](dataAnswer);
-}
-
 
 const populateBooksSection = () => {
     handleAPICalls('get-all-books', {});
---
+++ frontend/src/js/library/libraryHandlers.js
@@ -0,0 +1,207 @@
+import createHTMLElem from '../utils/domElements.js';
+import { clearInputs } from './libraryForms.js';
+import {
+    bookContainer, commentsList, addBookModal, addBookModalElem,
+    createCommentElement, createBookElement, displayBookInfo
+} from './library.js';
+
+
+export const errorHandler = (dataError, source) => {
+    const allBooksError = (errorAnswer) => {
+        let text = createHTMLElem['paragraph'](errorAnswer.error, 'm-1 text-center');
+        text.setAttribute('id', 'no-books-text');
+        bookContainer.removeChild(document.querySelector('#spinner-container-all-books'));
+        bookContainer.appendChild(text);
+    }
+    const bookInfoError = (errorAnswer) => {
+        displayBookInfo({
+            bookTitle: errorAnswer.error,
+            bookAuthor: '',
+            commentNodes: []
+        });
+    }
+    const addBookError = (errorAnswer) => {
+        const errorMessageBook = document.querySelector('#add-book-error-message');
+
+        const dismissMessageBook = () => errorMessageBook.textContent = '';
+
+        errorMessageBook.textContent = errorAnswer.error.slice(0, 1).toUpperCase() + errorAnswer.error.slice(1) + '!';
+        addBookModalElem.addEventListener('click', dismissMessageBook);
+    }
+    const addCommentError = (errorAnswer) => {
+        const errorMessageComment = document.querySelector('#book-info-error-message');
+    
+        const dismissMessageComment = () => errorMessageComment.textContent = '';
+
+        errorMessageComment.textContent = errorAnswer.error.slice(0, 1).toUpperCase() + errorAnswer.error.slice(1) + '!';
+        bookInfoModalElem.addEventListener('click', dismissMessageComment);
+    }
+
+    const errorsCallback = {
+        'get-all-books': allBooksError,
+        'get-book-info': bookInfoError,
+        'add-book-form': addBookError,
+        'add-comment-form': addCommentError
+    }
+    errorsCallback[source](dataError);
+}
+
+export const answerHandler = (dataAnswer, source) => {
+    const addBookAnswer = (answer) => {
+        if (document.querySelector('#no-books-text')) {
+            bookContainer.removeChild(document.querySelector('#no-books-text'));
+        }
+
+        let elem = createBookElement(answer);
+        bookContainer.insertBefore(elem, bookContainer.firstChild);
+        addBookModal.hide();
+    }
+    const addCommentAnswer = (answer) => {
+        const anchors = document.querySelectorAll('a');
+        for(let i = 0; i < anchors.length; i++) {
+            if (anchors[i].getAttribute('href') === answer[0]._id) {
+                anchors[i].innerHTML = `${answer[0].title}<br/>Author: ${answer[0].author}<br/>Comments: ${answer[0].commentcount}`;
+                break;
+            }
+        }
+    
+        const latestComment = answer[0].comments[answer[0].comments.length - 1];
+        let newComment = createCommentElement(latestComment);
+    
+        const listLine = commentsList.firstChild;
+        commentsList.insertBefore(newComment, listLine.nextSibling);
+    }
+    const allBooksAnswer = (answer) => {
+        if (document.querySelector('#spinner-container-all-books')) {
+            bookContainer.removeChild(document.querySelector('#spinner-container-all-books'));
+        }
+
+        answer.forEach((book) => {
+            let elem = createBookElement(book);
+            bookContainer.appendChild(elem);
+        });
+    }
+    const bookInfoAnswer = (answer) => {
+        const book = answer[0];
+        const commentSecNodes = [];
+
+        document.querySelector('#add-comment-form').setAttribute('action', book._id);
+    
+        let listLine = createHTMLElem['listItems']('', 'list-group-item mx-1 p-0', 'comment-first-li');
+        commentSecNodes.push(listLine);
+    
+        book.comments.sort((a, b) => new Date(b.created_on).getTime() - new Date(a.created_on).getTime())
+        .forEach((com) => {
+            let listElem = createCommentElement(com);
+            commentSecNodes.push(listElem);
+        });
+
+        displayBookInfo({
+            bookTitle: book.title,
+            bookAuthor: book.author,
+            commentNodes: commentSecNodes
+        });
+    }
+
+    const answerCallback = {
+        'get-all-books': allBooksAnswer,
+        'get-book-info': bookInfoAnswer,
+        'add-book-form': addBookAnswer,
+        'add-comment-form': addCommentAnswer
+    }
+    answerCallback[source](dataAnswer);
+}
+
+const handleAPICalls = (eventSource, data) => {
+    const readAllBooks = async () => {
+        try {
+            const reqBooks = await fetch('/personal-library/api/v1/books');
+            const answer = await reqBooks.json();
+
+            if (reqBooks.status === 500) {
+                throw { error: 'Something went wrong. Please try again later!' }
+            }
+
+            if (!answer.length) {
+                throw { error: 'No Books found!' }
+            }
+            answerHandler(answer, eventSource);
+        } catch(err) {
+            errorHandler(err, eventSource);
+        }
+    }
+
+    const readBook = async ({ bookID }) => {
+        try {
+            const reqBook = await fetch(`/personal-library/api/v1/books/${bookID}`);
+            const answer = await reqBook.json();
+
+            if (reqBook.status === 400) {
+                throw answer;
+            }
+            answerHandler(answer, eventSource);
+        } catch(err) {
+            errorHandler(err, eventSource);
+        }
+    }
+
+    const createBookComment = async ({ formData, bookID }) => {
+        const apis = {
+            'add-book-form': '/personal-library/api/v1/books',
+            'add-comment-form': `/personal-library/api/v1/books/${bookID}`
+        }
+
+        try {
+            const reqPost = await fetch(apis[eventSource], {
+                method: 'POST',
+                mode: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+            const answer = await reqPost.json();
+
+            if (reqPost.status === 400) {
+                throw answer;
+            }
+
+            clearInputs(eventSource);
+            answerHandler(answer, eventSource);
+        } catch(err) {
+            errorHandler(err, eventSource);
+        }
+    }
+
+    const deleteBooks = async () => {
+        try {
+            const reqDeletion = await fetch('/personal-library/api/v1/books', {
+                method: 'DELETE',
+                mode: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const answer = await reqDeletion.json();
+
+            if (reqDeletion.status === 400) {
+                throw answer;
+            }
+
+            console.log(answer);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    const actionsCalls = {
+        'get-all-books': readAllBooks,
+        'get-book-info': readBook,
+        'add-book-form': createBookComment,
+        'add-comment-form': createBookComment,
+        'delete-books': deleteBooks,
+    }
+    actionsCalls[eventSource](data);
+}
+
+export default handleAPICalls;
