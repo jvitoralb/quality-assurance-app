7a65a2a421585bcca231a37000e6325211b5e49d
jvitoralb <jvvalb@gmail.com>
2023-08-04 19:43:42 -0300

    NOTE: deployment config

--- gateway/components/gateway.js
+++ gateway/components/gateway.js
@@ -1,8 +1,10 @@
+import * as dotenv from 'dotenv';
 import proxy from 'express-http-proxy';
 
+dotenv.config();
 
 const gateway = (app) => {
-    app.use('/metric-converter', proxy('http://localhost:3003/'));
+    app.use('/metric-converter', proxy(process.env.SERVICE_03 || 'http://localhost:3003/'));
     app.use('/personal-library', proxy('http://localhost:3004/'));
     app.use('/sudoku-solver', proxy('http://localhost:3005/'));
     app.use('/issue-tracker', proxy('http://localhost:3006/'));
