8896ee7d5d0b9f379aa9cdfdd04cc9a1d3a1bead
jvitoralb <jvvalb@gmail.com>
2023-08-04 14:48:10 -0300

    fix: path configs

--- converter/lib/pathConfig.js
+++ converter/lib/pathConfig.js
@@ -3,6 +3,6 @@ import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 export const __dirname = dirname(__filename)
-const pathFrontend = join(__dirname, '..', '..', '..','frontend')
+const pathFrontend = join(__dirname, '..', '..','frontend')
 
 export default pathFrontend
